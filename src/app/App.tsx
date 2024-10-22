6365a87cfdf6c485d68be7278a8c9fa92f82d8b2
Baryshewww <baryshev-tm@mail.ru>
2024-10-22 08:55:20 +0300

    feat(project): подключить все приложение в index, и проверить как работает

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,29 @@
+import styled from 'styled-components';
+
+import { TasksProvider } from './provider/StoreProvider';
+import GlobalStyles from './styles/globalStyles';
+
+import { MainPage } from '@/pages/MainPage';
+
+const AppStyled = styled.div`
+    min-height: 100%;
+    display: flex;
+    flex-direction: column;
+    gap: 80px;
+    padding: 70px 0 40px;
+    margin: 0 auto;
+    width: 649px;
+`;
+
 export const App = () => {
-    return <h1>Hello!</h1>
+    return (
+        <TasksProvider>
+            <GlobalStyles />
+            <AppStyled>
+                <MainPage />
+            </AppStyled>
+        </TasksProvider>
+    );
 };
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import { createRoot } from 'react-dom/client';
 
-import App from "@/app/App";
+import App from '@/app/App';
+// @ts-expect-error Это подключение шрифтов
+import FontStyles from '@/app/styles/fontStyles';
 
 const container = document.getElementById('root');
 
@@ -11,5 +13,5 @@ if (!container) {
 const root = createRoot(container);
 
 root.render(
-    <App/>
+    <App />,
 );
